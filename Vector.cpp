6a13c81abe069e5ed42ed5f981475c96cdc354e3
i-khadra <saeed.developer18@gmail.com>
2022-10-19 11:28:33 +0200

    updated the Vector file: adding some functions and homework solutions

--- Vector.cpp
+++ Vector.cpp
@@ -4,11 +4,13 @@ using namespace std;
 class Vector {
 private:
     int* arr = nullptr;
-    int size;
+    int size; // filled size
+    int capacity; // actual allocated size
 public:
     Vector(int size) {
         this->size = size < 0? 1 : size;
-        arr = new int[size] {}; // initialized with 0 by default
+        capacity = 2*size;
+        arr = new int[capacity] {}; // initialized with 0 by default
     }
     ~Vector() {
         delete[] arr;
@@ -53,7 +55,19 @@ public:
         return arr[size - 1];
     }
 
-    void Push_back(int element) {
+    void expand_capacity() {
+        capacity = 2*size;
+        int* tmp = new int[capacity];
+        for (int i = 0; i < size; ++i) {
+            tmp[i] = arr[i];
+        }
+
+        swap(arr, tmp);
+        delete[] tmp;
+    }
+    void push_back(int element) {
+        if(size == capacity) expand_capacity();
+
         arr[size] = element;
         size++;
     }
@@ -61,17 +75,91 @@ public:
     void insert(int idx, int val) {
         // worst case: O(size)
         assert(idx >= 0 and idx < size);
+
+        if(size == capacity) expand_capacity();
         size++;
         for(int i = size - 1; i > idx; --i)
             arr[i] = arr[i - 1];
         arr[idx] = val;
     }
+
+    /// challenges solutions ///
+    void right_rotate() {
+        int last = arr[size - 1];
+        for (int i = size - 1; i >= 1; --i) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+    } // O(size)
+    void left_rotate() {
+        int first = arr[0];
+        for (int i = 0; i < size - 1; ++i) {
+            arr[i] = arr[i + 1];
+        }
+        arr[size - 1] = first;
+    } // O(size)
+
+    void right_rotate(int times) {
+        int steps = times % size;
+        int toShift = size - steps;
+
+        if (steps > 0) {
+            int *last_elements = new int[steps];
+            for (int i = 0; i < steps; ++i) {
+                last_elements[i] = arr[i + toShift];
+            }
+
+            // shift elements:
+            for (int i = size - 1; i >= steps; --i) {
+                arr[i] = arr[i - steps];
+            }
+
+            // add last elements:
+            for (int i = 0; i < steps; ++i) {
+                arr[i] = last_elements[i];
+            }
+        }
+    } // O(size)
+
+    int pop(int idx) {
+        assert(idx >= 0 and idx < size);
+
+        int ret = arr[idx];
+        // remove:
+        for (int i = idx; i < size - 1; ++i) {
+            arr[i] = arr[i + 1]; // shift right elements to the left
+        }
+        size--;
+
+        return ret;
+    }
+
+    int find_transposition(int value) {
+        for (int i = 0; i < size; ++i) {
+            if(arr[i] == value) {
+                if(i > 0) swap(arr[i], arr[i - 1]); // shift it to the left
+                return max(0, i - 1);
+            }
+        }
+
+        return -1; // not found
+    } // O(size), but with enhanced shifting trick
+
 };
 
 int main()
 {
-    Vector vec(10);
-    vec.print();
+    Vector v(6);
+    for (int i = 0; i < 6; ++i) {
+        v.set(i, i + 1);
+    }
+    v.right_rotate(9);
+    v.print();
 
+    cout << v.pop(5) << endl;
+    v.print();
+
+    cout << v.find_transposition(10) << endl;
+    v.print();
     return 0;
 }
